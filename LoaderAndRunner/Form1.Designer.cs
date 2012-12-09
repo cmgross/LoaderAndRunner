9c3c5541a238c3cdb8c105dab777549bc2566723
Uziel <chuckgross@gmail.com>
2012-12-09 11:39:47 -0500

    First commit

---
+++ LoaderAndRunner/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace LoaderAndRunner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.itemsToDo = new System.Windows.Forms.ListView();
+            this.itemsCompleted = new System.Windows.Forms.ListView();
+            this.run = new System.Windows.Forms.Button();
+            this.loading = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // itemsToDo
+            // 
+            this.itemsToDo.Location = new System.Drawing.Point(12, 12);
+            this.itemsToDo.Name = "itemsToDo";
+            this.itemsToDo.Size = new System.Drawing.Size(121, 97);
+            this.itemsToDo.TabIndex = 0;
+            this.itemsToDo.UseCompatibleStateImageBehavior = false;
+            // 
+            // itemsCompleted
+            // 
+            this.itemsCompleted.Location = new System.Drawing.Point(276, 12);
+            this.itemsCompleted.Name = "itemsCompleted";
+            this.itemsCompleted.Size = new System.Drawing.Size(121, 97);
+            this.itemsCompleted.TabIndex = 1;
+            this.itemsCompleted.UseCompatibleStateImageBehavior = false;
+            // 
+            // run
+            // 
+            this.run.Location = new System.Drawing.Point(152, 12);
+            this.run.Name = "run";
+            this.run.Size = new System.Drawing.Size(100, 25);
+            this.run.TabIndex = 2;
+            this.run.Text = "Run";
+            this.run.UseVisualStyleBackColor = true;
+            this.run.Click += new System.EventHandler(this.run_Click);
+            // 
+            // loading
+            // 
+            this.loading.Location = new System.Drawing.Point(152, 43);
+            this.loading.Name = "loading";
+            this.loading.Size = new System.Drawing.Size(100, 25);
+            this.loading.Style = System.Windows.Forms.ProgressBarStyle.Marquee;
+            this.loading.TabIndex = 3;
+            this.loading.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(409, 136);
+            this.Controls.Add(this.loading);
+            this.Controls.Add(this.run);
+            this.Controls.Add(this.itemsCompleted);
+            this.Controls.Add(this.itemsToDo);
+            this.Name = "Form1";
+            this.Text = "LoadAndRun";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView itemsToDo;
+        private System.Windows.Forms.ListView itemsCompleted;
+        private System.Windows.Forms.Button run;
+        private System.Windows.Forms.ProgressBar loading;
+    }
+}
+
---
+++ LoaderAndRunner/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LoaderAndRunner
+{
+    public partial class Form1 : Form
+    {
+        private BlockingCollection<ListViewItem> _itemsToLoad;
+        private BlockingCollection<ListViewItem> _itemsToRun;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            _itemsToLoad = new BlockingCollection<ListViewItem>();
+            _itemsToRun = new BlockingCollection<ListViewItem>();
+
+            var item1 = new ListViewItem("Item1");
+            var item2 = new ListViewItem("Item2");
+            var item3 = new ListViewItem("Item3");
+            var item4 = new ListViewItem("Item4");
+            
+
+            itemsToDo.Items.Add(item1);
+            itemsToDo.Items.Add(item2);
+            itemsToDo.Items.Add(item3);
+            itemsToDo.Items.Add(item4);
+            _itemsToLoad.Add(item1);
+            _itemsToLoad.Add(item2);
+            _itemsToLoad.Add(item3);
+            _itemsToLoad.Add(item4);
+
+            _itemsToLoad.CompleteAdding();
+        }
+
+        private void run_Click(object sender, EventArgs e)
+        {
+            StartBackgroundTasks();
+            TaskIsRunning();
+        }
+
+        private void StartBackgroundTasks()
+        {
+            Task loader1 = Task.Factory.StartNew(() =>
+            {
+                foreach (ListViewItem item in _itemsToLoad.GetConsumingEnumerable())
+                {
+                    //consuming an item should remove it from the collection. 
+                    //since _itemsToLoad is set as complete adding, once this list has been enumerated through it should gracefully exit
+
+                    //simulate object loading from database that is long running
+                    Debug.WriteLine("Loader 1 says: loading item: " + item.Text + " started at: " + DateTime.Now);
+                    Thread.Sleep(2000);
+                    Debug.WriteLine("Loader 1 says: loading item: " + item.Text + " completed at " + DateTime.Now);
+                    _itemsToRun.Add(item);
+                }
+                Debug.WriteLine("Loader 1 says: Nothing left to load!");
+            }, TaskCreationOptions.LongRunning);
+
+            Task loader2 = Task.Factory.StartNew(() =>
+            {
+                foreach (ListViewItem item in _itemsToLoad.GetConsumingEnumerable())
+                {
+                    //consuming an item should remove it from the collection. 
+                    //since _itemsToLoad is set as complete adding, once this list has been enumerated through it should gracefully exit
+
+                    //simulate loading from database that is long running
+                    Debug.WriteLine("Loader 2 says: loading item: " + item.Text + " started at: " + DateTime.Now);
+                    Thread.Sleep(2000);
+                    Debug.WriteLine("Loader 2 says: loading item: " + item.Text + " completed at " + DateTime.Now);
+                    _itemsToRun.Add(item);
+                }
+                Debug.WriteLine("Loader 2 says: nothing left to load!");
+            }, TaskCreationOptions.LongRunning);
+
+            Task runner1 = Task.Factory.StartNew(() =>
+            {
+                foreach (ListViewItem item in _itemsToRun.GetConsumingEnumerable())
+                {
+                    //consuming an item should remove it from the collection. 
+                    //since _itemsToRun is set as complete adding, once this list has been enumerated through it should gracefully exit
+
+                    //simulate writing information to mainframe emulator
+                    Debug.WriteLine("Runner 1 says: running item: " + item.Text + " started at: " + DateTime.Now);
+                    Thread.Sleep(2000);
+                    Debug.WriteLine("Runner 1 says: running item: " + item.Text + " completed at " + DateTime.Now);
+
+                    ListViewItem item1 = item;
+                    itemsToDo.Invoke(new MethodInvoker(() => itemsToDo.Items.Remove(item1)));
+                    Debug.WriteLine("Runner 1 removed item: " + item1.Text + " from ToDo Listview at " + DateTime.Now);
+                    itemsCompleted.Invoke(new MethodInvoker(() => itemsCompleted.Items.Add(item1)));
+                    Debug.WriteLine("Runner 1 added item: " + item1.Text + " to Completed Listview at " + DateTime.Now);
+                }
+                Debug.WriteLine("Runner 1 says: Nothing left to run!");
+            }, TaskCreationOptions.LongRunning);
+
+            //Task.WaitAll(loader1, loader2, runner1);
+            Task.Factory.ContinueWhenAll(new[] { loader1, loader2 }, completedTasks => _itemsToRun.CompleteAdding());
+            Task.Factory.ContinueWhenAll(new[] { runner1 }, completedTasks => TaskIsComplete());
+        }
+
+        private void TaskIsRunning()
+        {
+            // Update UI to reflect background task.
+            run.Enabled = false;
+            loading.Visible = true;
+            loading.MarqueeAnimationSpeed = 30;
+        }
+
+        private void TaskIsComplete()
+        {
+            // Reset UI.
+            run.Invoke(new MethodInvoker(() => run.Enabled = true));
+            loading.Invoke(new MethodInvoker(() => loading.Visible = false));
+        }
+    }
+}
